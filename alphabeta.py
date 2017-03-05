deab3944b36b9c76b932cae19ce3fe9982e91ef1
ykl7 <yklal95@gmail.com>
2017-03-05 17:30:25 +0530

    TicTacToe class implementation

--- alphabeta.py
+++ alphabeta.py
@@ -33,6 +33,73 @@ class Game:
         return [(move, self.play_move(move, state))
                 for move in self.moves_possible(state)]
 
+class TicTacToe(Game):
+    """
+    Player entries : X and O
+    Default board size 3x3
+    """
+    def __init__(self, h=3, v=3, k=3):
+        update(self, h=h, v=v, k=k)
+        moves = [(x, y) for x in range(1, h+1)
+                 for y in range(1, v+1)]
+        self.initial = Struct(to_move='X', utility=0, board={}, moves=moves)
+
+    def moves_possible(self, state):
+        return state.moves
+
+    def play_move(self, move, state):
+        if move not in state.moves:
+            return state
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return Struct(to_move=if_(state.to_move == 'X', 'O', 'X'), utility=self.check_state(board, move, state.to_move), board=board, moves=moves)
+
+    def current_state(self, state):
+        """
+        1 for win, -1 for loss, 0 for illegal
+        """
+        return state.utility
+
+    def terminate(self, state):
+        """
+        if game won or no empty squares
+        """
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h+1):
+            for y in range(1, self.v+1):
+                print board.get((x, y), '.'),
+            print
+
+    def check_state(self, board, move, player):
+        """
+        If X wins with this move, return 1; if O wins, return -1; else return 0.
+        """
+        if (self.line_through_move(board, move, player, (0, 1)) or self.line_through_move(board, move, player, (1, 0)) or self.line_through_move(board, move, player, (1, -1)) or self.line_through_move(board, move, player, (1, 1))):
+            return if_(player == 'X', +1, -1)
+        else:
+            return 0
+
+    def line_through_move(self, board, move, player, (dx, dy)):
+        """
+        Line through board move available
+        """
+        x, y = move
+        n = 0
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + dx, y + dy
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - dx, y - dy
+        n -= 1
+        return n >= self.k
+
 def max_full_value(state, alpha, beta, game):
         if game.terminate(state):
             return game.current_state(state, player)
